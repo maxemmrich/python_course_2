82c84c2ba62c46781f4c1503615999783219ed12
maxemmrich <max.emmrich@t-online.de>
2020-10-29 18:20:57 +0100

    Create is_even function

---
+++ pycharm_features.py
@@ -0,0 +1,14 @@
+
+def is_even(x: int) -> bool:
+
+    '''
+    Checks if input is even
+
+    :param x:
+    :return: boolean
+    '''
+
+    if x % 2 == 0:
+        return True
+    else:
+        return False
